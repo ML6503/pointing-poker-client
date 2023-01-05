879e967b27c176d5eee35425407a06a94b65af60
ML6503 <ml6503@gmail.com>
2023-01-05 18:18:46 +0200

    fix: crypto hash pro

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+const crypto = require('crypto');
+
+try {
+  crypto.createHash('md4');
+} catch (e) {
+  console.warn('Crypto "md4" is not supported anymore by this Node version');
+  const origCreateHash = crypto.createHash;
+  crypto.createHash = (alg, opts) => {
+    return origCreateHash(alg === 'md4' ? 'md5' : alg, opts);
+  };
+}
+
 module.exports = {
   root:'',
   reactStrictMode: false,
-  requireConfigFile: false
+  requireConfigFile: false,
 }
